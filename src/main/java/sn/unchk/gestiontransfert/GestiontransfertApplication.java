8e4d1b8c666126e9070aff66f2ad3755db84cb11
ZahraThiaw <fatimatathiaw6@gmail.com>
2025-08-31 20:53:15 +0000

    initialisation du projet

---
+++ src/main/java/sn/unchk/gestiontransfert/GestiontransfertApplication.java
@@ -0,0 +1,13 @@
+package sn.unchk.gestiontransfert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiontransfertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiontransfertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/unchk/gestiontransfert/GestiontransfertApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.unchk.gestiontransfert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiontransfertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
